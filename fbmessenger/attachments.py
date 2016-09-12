365ab676bad461ad0df038e2541747c0a7097845
Ricky Dunlop <hello@rickydunlop.co.uk>
2016-09-12 23:17:09 +0100

    Support reuse of attachments by attachment_id

--- fbmessenger/attachments.py
+++ fbmessenger/attachments.py
@@ -4,13 +4,12 @@ from .quick_replies import QuickReplies
 
 
 class BaseAttachment(object):
-    def __init__(self, attachment_type, url, is_reusable=None, quick_replies=None):
-        self.type = attachment_type
+    def __init__(self, attachment_type, url=None, is_reusable=None,
+                 quick_replies=None, attachment_id=None):
+        self.attachment_type = attachment_type
         self.url = url
-
-        if is_reusable is None:
-            is_reusable = False
         self.is_reusable = is_reusable
+        self.attachment_id = attachment_id
 
         if quick_replies and not isinstance(quick_replies, QuickReplies):
             raise TypeError('quick_replies must be an instance of QuickReplies.')
@@ -19,15 +18,19 @@ class BaseAttachment(object):
     def to_dict(self):
         d = {
             'attachment': {
-                'type': self.type,
-                'payload': {
-                    'url': self.url
-                }
+                'type': self.attachment_type,
+                'payload': {}
             }
         }
 
+        if self.url:
+            d['attachment']['payload']['url'] = self.url
+
         if self.is_reusable:
             d['attachment']['payload']['is_reusable'] = 'true'
+
+        if self.attachment_id:
+            d['attachment']['payload']['attachment_id'] = self.attachment_id
 
         if self.quick_replies:
             d['attachment']['quick_replies'] = self.quick_replies.to_dict()
@@ -36,29 +39,44 @@ class BaseAttachment(object):
 
 
 class Image(BaseAttachment):
-    def __init__(self, url, quick_replies=None):
+    def __init__(self, url, is_reusable=None, quick_replies=None, attachment_id=None):
         self.attachment_type = 'image'
         self.url = url
+        self.is_reusable = is_reusable
         self.quick_replies = quick_replies
-        super(Image, self).__init__(self.attachment_type, self.url, self.quick_replies)
+        self.attachment_id = attachment_id
+        super(Image, self).__init__(self.attachment_type, self.url, self.is_reusable,
+                                    self.quick_replies, self.attachment_id)
 
 
 class Audio(BaseAttachment):
-    def __init__(self, url):
+    def __init__(self, url, is_reusable=None, quick_replies=None, attachment_id=None):
         self.attachment_type = 'audio'
         self.url = url
-        super(Audio, self).__init__(self.attachment_type, self.url)
+        self.is_reusable = is_reusable
+        self.quick_replies = quick_replies
+        self.attachment_id = attachment_id
+        super(Audio, self).__init__(self.attachment_type, self.url, self.is_reusable,
+                                    self.quick_replies, self.attachment_id)
 
 
 class Video(BaseAttachment):
-    def __init__(self, url):
+    def __init__(self, url, is_reusable=None, quick_replies=None, attachment_id=None):
         self.attachment_type = 'video'
         self.url = url
-        super(Video, self).__init__(self.attachment_type, self.url)
+        self.is_reusable = is_reusable
+        self.quick_replies = quick_replies
+        self.attachment_id = attachment_id
+        super(Video, self).__init__(self.attachment_type, self.url, self.is_reusable,
+                                    self.quick_replies, self.attachment_id)
 
 
 class File(BaseAttachment):
-    def __init__(self, url):
+    def __init__(self, url, is_reusable=None, quick_replies=None, attachment_id=None):
         self.attachment_type = 'file'
         self.url = url
-        super(File, self).__init__(self.attachment_type, self.url)
+        self.is_reusable = is_reusable
+        self.quick_replies = quick_replies
+        self.attachment_id = attachment_id
+        super(File, self).__init__(self.attachment_type, self.url, self.is_reusable,
+                                   self.quick_replies, self.attachment_id)

fd9a4b0f647d275bb3185d695816cc7169c2c335
Victor Gavro <vgavro@gmail.com>
2022-07-29 00:27:50 +0300

    setup: fixed install and test dependencies

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ test_requirements = [
     'coverage',
     'pytest-cov',
     'pytest-catchlog',
-    'responses'
+    'responses',
+    'mock',
 ]
 
 # Get the version
@@ -78,7 +79,10 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    install_requires=['requests>=2.0'],
+    install_requires=[
+        'requests>=2.0',
+        'six',
+    ],
     packages=['fbmessenger'],
     cmdclass={'test': PyTest},
     tests_require=test_requirements,
